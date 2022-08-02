42528904a4060d1ba4c25ff84c215910f4e3695a
Dominick Cortez <jdcortez2268@gmail.com>
2022-08-01 21:45:31 -0700

    aug 1

--- promises-practice.js
+++ promises-practice.js
@@ -67,3 +67,4 @@ fetchUsers();
 // const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then( res => res.json());
 // // .then is called when all promises are resolved
 // Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
+
